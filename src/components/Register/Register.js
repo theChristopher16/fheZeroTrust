1036b2f446831c1b93af4d86ad327b6c21c19ada
Christopher Smith <christophersmithm16@gmail.com>
2023-01-27 10:12:14 -0600

    added additional fields to registration component. also forgot to mention that i used react routes for routing to component to component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,23 +1,92 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Register.css";
 
 function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [dob, setDob] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(email, password, firstName, lastName, phone, address, dob);
+  }
+
   return (
-    <div>
-      <h1>Please Register</h1>
-      <form>
+    <div className="form-container">
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
         <label>
           Email:
-          <input type="email" name="email" required/>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </label>
         <br />
         <label>
           Password:
-          <input type="password" name="password" required/>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </label>
         <br />
         <label>
-          Confirm Password:
-          <input type="password" name="confirmPassword" required/>
+          First Name:
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Phone:
+          <input
+            type="tel"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Address:
+          <input
+            type="text"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Date of Birth:
+          <input
+            type="date"
+            value={dob}
+            onChange={(e) => setDob(e.target.value)}
+            required
+          />
         </label>
         <br />
         <button type="submit">Submit</button>
