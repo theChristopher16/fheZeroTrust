3d82e5ca214c0e5cc5661455da4c539e44d65f3e
Christopher Smith <christophersmithm16@gmail.com>
2023-01-31 11:27:02 -0600

    Added frontend hashing to pass off to backend.

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,26 +1,48 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
+import { hash } from 'bcryptjs';
+import React, { useState } from 'react';
+import { hashPassword } from '../../util/hash';
 import './Login.css';
 
-function Login() {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const hashedPassword = await hashPassword(password);
+    console.log(hashedPassword);
+  };
+
   return (
     <div>
       <h1>Please Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Email:
-          <input type="email" name="email" required/>
+          <input
+            type="email"
+            name="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <br />
         <label>
           Password:
-          <input type="password" name="password" required/>
+          <input
+            type="password"
+            name="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <br />
         <button type="submit">Submit</button>
       </form>
     </div>
   );
-}
+};
 
 export default Login;
